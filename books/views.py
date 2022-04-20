ef905f26f1116057241f46b0da4dac0138c22a7a
Tomasz <tomasz1577@gmail.com>
2022-04-20 18:47:15 +0200

    Added path-args

--- books/views.py
+++ books/views.py
@@ -13,6 +13,12 @@ def get_uuids_a(request: WSGIRequest) -> HttpResponse:
     uuids = [f"{uuid4()}" for _ in range(10)]
     return HttpResponse(f"uuids={uuids}")
 
+
 def get_uuids_b(request: WSGIRequest) -> JsonResponse:
     uuids = [f"{uuid4()}" for _ in range(10)]
-    return JsonResponse({"uuids":uuids})
+    return JsonResponse({"uuids": uuids})
+
+
+def get_argument_from_path(request: WSGIRequest, x: int, y: str, z: str) -> HttpResponse:
+
+    return HttpResponse(f"X={x}, Y={y}, Z={z}")
