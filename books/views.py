73dbeb08706e889094b355fec80b1500dd233802
Tomasz <tomasz1577@gmail.com>
2022-04-21 18:57:16 +0200

    Exercise FBV

--- books/views.py
+++ books/views.py
@@ -1,8 +1,10 @@
 from uuid import uuid4
 
+from django.core.exceptions import BadRequest
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 def get_hello(request: WSGIRequest) -> HttpResponse:
@@ -20,5 +22,38 @@ def get_uuids_b(request: WSGIRequest) -> JsonResponse:
 
 
 def get_argument_from_path(request: WSGIRequest, x: int, y: str, z: str) -> HttpResponse:
-
     return HttpResponse(f"X={x}, Y={y}, Z={z}")
+
+
+def get_arguments_from_query(request: WSGIRequest) -> HttpResponse:
+    a = request.GET.get("a")
+    b = request.GET.get("b")
+    c = request.GET.get("c")
+    print(type(int(a)))
+    return HttpResponse(f"a={a}, b={b}, c={c}")
+
+
+@csrf_exempt
+def check_http_query_type(request: WSGIRequest) -> HttpResponse:
+    query_type = "Unknown"
+    if request.method == "GET":
+        query_type = "This is GET"
+    elif request.method == "POST":
+        query_type = "This is POST"
+    elif request.method == "PUT":
+        query_type = "This is PUT"
+    elif request.method == "DELETE":
+        query_type = "This is DELETE"
+
+    return HttpResponse(query_type)
+
+
+def get_headers(request: WSGIRequest) -> JsonResponse:
+    our_headers = request.headers
+    return JsonResponse({"Test": dict(our_headers)})
+
+@csrf_exempt
+def raise_error_for_fun(request: WSGIRequest) -> HttpResponse:
+    if request.method != "GET":
+        raise BadRequest("Method not allowed")
+    return HttpResponse("Wszystko OK!")
