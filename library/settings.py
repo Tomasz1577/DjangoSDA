3f40c25eb3c2e3a32a6acbcb75923155886414ae
Tomasz <tomasz1577@gmail.com>
2022-04-21 19:16:04 +0200

    Cleaning urls

--- library/settings.py
+++ library/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6#w^mz8$7b%zqukfe^&=rm@txl8&mr8i7$eo8+b_8ci5kyg7ou'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
