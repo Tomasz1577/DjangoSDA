91610c559a68cfb2f0764efa9daca8e9bd73a442
Tomasz <tomasz1577@gmail.com>
2022-04-23 08:02:16 +0200

    Changes uuids-b to uuids-a

--- books/views.py
+++ books/views.py
@@ -14,13 +14,13 @@ def get_hello(request: WSGIRequest) -> HttpResponse:
 
 def get_uuids_a(request: WSGIRequest) -> HttpResponse:
     uuids = [f"{uuid4()}" for _ in range(10)]
-    return HttpResponse(f"uuids={uuids}")
+    return render(request, template_name="uuids_a.html", context={"elements": uuids})
+    # return HttpResponse(f"uuids={uuids}")
 
 
 def get_uuids_b(request: WSGIRequest) -> JsonResponse:
     uuids = [f"{uuid4()}" for _ in range(10)]
-    return render(request, template_name="uuids_a.html", context={"elements": uuids})
-    # return JsonResponse({"uuids": uuids})
+    return JsonResponse({"uuids": uuids})
 
 
 def get_argument_from_path(request: WSGIRequest, x: int, y: str, z: str) -> HttpResponse:
