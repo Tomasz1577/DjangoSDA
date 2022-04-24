9c513eec7de7eee95496b47f8972a39579095d2c
Tomasz <tomasz1577@gmail.com>
2022-04-24 12:06:01 +0200

    Added books_list and pagination views

--- books/views.py
+++ books/views.py
@@ -6,9 +6,9 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
-from books.models import BookAuthor, Category
+from books.models import BookAuthor, Category, Book
 
 
 class AuthorListBaseView(View):
@@ -23,6 +23,12 @@ class AuthorListBaseView(View):
 class CategoryListTemplateView(TemplateView):
     template_name = "category_list.html"
     extra_context = {"categories": Category.objects.all()}  # type: ignore
+
+
+class BooksListView(ListView):
+    template_name = "books_list.html"
+    model = Book
+    paginate_by = 10
 
 
 
