c12ad218e3e27caf0cfdeeb81d6c0360618e6071
Tomasz <tomasz1577@gmail.com>
2022-05-14 14:20:36 +0200

    New hash

--- users/utils.py
+++ users/utils.py
@@ -1,9 +1,28 @@
 import hashlib
+import os
 
+#hashowanie hasła
 def hash_password_without_salt(password: str ) -> str:
     hash_password = hashlib.sha256(password.encode("ascii")).hexdigest()
     return hash_password
 
+
+#porównanie czy hasło wprowadzone a hasło hashowane jest takie samo
 def verification_password_without_salt(password: str, hash_password: str) -> bool:
     input_password = hashlib.sha256(password.encode("ascii")).hexdigest()
     return input_password == hash_password
+
+def hash_password_with_salt(password: str) -> str:
+    #creating salt
+    random_size = 30
+    salt = os.urandom(random_size)
+    hashed_salt = hashlib.sha256(salt).hexdigest()
+
+    #hashing password
+    new_password = password + hashed_salt
+    hashed_passowrd = hashlib.sha256(new_password.encode('ascii')).hexdigest()
+
+    combined_salt_and_password = ''.join([hashed_passowrd, hashed_salt])
+
+    return combined_salt_and_password
+
